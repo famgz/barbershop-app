228b0d86314efa00d6c7b0f3a7d8f80bebdfe0ab
famgz <famgz@proton.me>
2024-01-30 21:44:27 -0300

    feat: initial header component

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import { Card, CardContent } from './ui/card';
+import { Button } from './ui/button';
+import { MenuIcon } from 'lucide-react';
+
+export default function Header() {
+  return (
+    <Card>
+      <CardContent className='py-8 px-5 flex items-center justify-between mt'>
+        <Image src={'/logo.png'} alt='Logo Barbearia' height={22} width={120} />
+        <Button variant='outline' size='icon' className='size-8'>
+          <MenuIcon size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
