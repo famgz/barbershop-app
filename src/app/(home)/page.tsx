0bde1b2cd6e1976f7ef829693babaef6adcb94f8
famgz <famgz@proton.me>
2024-01-30 22:10:00 -0300

    feat: add initial search component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,19 @@
 import { format } from 'date-fns';
+import { ptBR } from "date-fns/locale";
+import Search from './_components/search';
+
 export default function Home() {
   return (
     <div>
       <div className='px-5 pt-5'>
         <h2 className='text-xl font-bold'>Ola, Aluno!</h2>
         <p className='capitalize text-sm'>
-          {format(new Date(), `EEEE',' dd 'de' MMMM`)}
+          {format(new Date(), `EEEE',' dd 'de' MMMM`, {locale: ptBR})}
         </p>
+      </div>
+
+      <div className='px-5 mt-6'>
+      <Search />
       </div>
     </div>
   );
