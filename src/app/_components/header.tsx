806640264929af5728e1415b4bf6a5ff0466cafa
famgz <famgz@proton.me>
2024-02-04 10:52:58 -0300

    fix: header logo redirect to home page

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,32 +1,29 @@
-import {
-  MenuIcon
-} from 'lucide-react';
+import { MenuIcon } from 'lucide-react';
 import Image from 'next/image';
 import SideMenu from './side-menu';
 import { Button } from './ui/button';
 import { Card, CardContent } from './ui/card';
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger
-} from './ui/sheet';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header>
       <Card>
-        <CardContent className='py-8 px-5 flex items-center justify-between mt'>
-          <Image
-            src={'/logo.png'}
-            alt='Logo Barbearia'
-            height={22}
-            width={120}
-          />
+        <CardContent className='py-4 px-5 flex items-center justify-between mt'>
+          <Link href={'/'}>
+            <Image
+              src={'/logo.png'}
+              alt='Logo Barbearia'
+              height={22}
+              width={120}
+            />
+          </Link>
 
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant='outline' size='icon' className='size-10'>
-                <MenuIcon size={20} />
+              <Button variant='outline' size='icon' className='size-8'>
+                <MenuIcon size={16} />
               </Button>
             </SheetTrigger>
 
@@ -34,7 +31,6 @@ export default function Header() {
               <SideMenu />
             </SheetContent>
           </Sheet>
-          
         </CardContent>
       </Card>
     </header>
